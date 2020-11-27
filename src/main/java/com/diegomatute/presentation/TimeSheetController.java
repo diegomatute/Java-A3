0d66079e45c718a9e6a11a15cbd8c9f8ed736946
Diego Matute <Dmatute43@gmail.com>
2020-11-27 17:09:46 -0500

    Added TimeSheet and shift.json files. Updated Main.fxml file to output two tabs, and a menu. Added text under the worker tab and test text for the listview under the Shifts tab.

--- src/main/java/com/diegomatute/presentation/TimeSheetController.java
+++ src/main/java/com/diegomatute/presentation/TimeSheetController.java
@@ -5,11 +5,40 @@
  */
 package com.diegomatute.presentation;
 
+import com.diegomatute.business.TimeSheet;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+
+
 /**
  *
  * @author diegomatute
  */
 public class TimeSheetController 
 {
+    
+    private TimeSheet ts = new TimeSheet();
+    
+    
+    @FXML
+    private AnchorPane anchorpane;
+    
+    @FXML
+    private MenuItem openMenuItem;
+    
+    @FXML
+    private MenuItem saveAsMenuItem;
+    
+    @FXML
+    private MenuItem saveReportMenuItem;
+    
+    @FXML
+    private MenuItem exitMenuItem;
+    
+    
+    @FXML
+    private ListView listView;
     
 }
