8a95c14593b960f11850b89a91443d621bb636e0
Diego Matute <Dmatute43@gmail.com>
2020-11-29 13:35:41 -0500

    Third update. Controller and Main.fxml files updated to open, save as, and save files.

--- src/main/java/com/diegomatute/presentation/TimeSheetController.java
+++ src/main/java/com/diegomatute/presentation/TimeSheetController.java
@@ -6,10 +6,18 @@
 package com.diegomatute.presentation;
 
 import com.diegomatute.business.TimeSheet;
+import java.io.File;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.Tab;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Window;
+import javafx.stage.WindowEvent;
 
 
 /**
@@ -41,4 +49,72 @@ public class TimeSheetController
     @FXML
     private ListView listView;
     
+    @FXML
+    private Tab workerTab;
+    
+    @FXML
+    private Tab shiftTab;
+    
+    @FXML
+    private Window stage;
+    
+   @FXML
+   protected void handleOpenMenuItemAction(ActionEvent event)
+   {
+       //System.out.println("Opened file chooser");
+       
+       FileChooser fileChooser = new FileChooser();
+       
+       fileChooser.setTitle("Open TimeSheet File");
+       
+       File file = fileChooser.showOpenDialog(stage);
+       
+       /*
+       if (file != null)
+       {
+           stage.display(file);
+       }
+        */
+       
+       
+       // https://docs.oracle.com/javafx/2/ui_controls/file-chooser.htm
+   }
+   
+   @FXML
+   protected void handleSaveAsMenuItemAction(ActionEvent event)
+   {
+       
+       FileChooser fileChooser = new FileChooser();
+       
+       fileChooser.setTitle("Save As TimeSheet");
+       
+       //fileChooser.setSelectedExtensionFilter(new FileChooser.ExtensionFilter("Json files(*.json)", "*.json"));
+       fileChooser.getExtensionFilters().addAll(
+       new ExtensionFilter("Text Files", "*.txt"),
+       new ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"),
+       new ExtensionFilter("Json Files", "*.json"));
+       File file = fileChooser.showSaveDialog(stage);
+       
+   }
+    
+   @FXML
+   protected void handleSaveReportMenuItemAction(ActionEvent event)
+   {
+       FileChooser fileChooser = new FileChooser();
+       
+       fileChooser.setTitle("Save TimeSheet");
+       
+       
+   }
+   
+   @FXML
+   protected void handleExitMenuItemAction(WindowEvent event)
+   {
+      /* 
+      Platform.exit();
+      System.exit(0);
+      */
+   }
+   
+   
 }
